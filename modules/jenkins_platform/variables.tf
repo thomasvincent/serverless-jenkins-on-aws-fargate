889100a9cb340096524a1b5e44f56eeb3e8e4950
Thomas Vincent <thomasvincent@gmail.com>
2024-05-03 21:39:12 -0700

    docs: Improve variable definitions and add descriptions
    - Added a description for each variable to clarify its purpose
    - Updated variable descriptions to use sentence case and provide more details
    - Removed unnecessary comments and formatted the code for better readability
    - Renamed the `tags` variable type to `map(string)` for consistency
    - Reordered variables to group related ones together and improve logical flow
    
    The added descriptions serve as better documentation for users of the module, making it easier to understand the purpose and usage of each variable.

--- modules/jenkins_platform/variables.tf
+++ modules/jenkins_platform/variables.tf
@@ -1,229 +1,231 @@
-
-variable jenkins_ecr_repository_name {
+variable "jenkins_ecr_repository_name" {
   type        = string
   default     = "serverless-jenkins-controller"
   description = "Name for Jenkins controller ECR repository"
 }
 
-
-variable name_prefix {
-  type    = string
-  default = "serverless-jenkins"
+variable "name_prefix" {
+  type        = string
+  default     = "serverless-jenkins"
+  description = "Prefix to be used in the name of resources"
 }
 
-variable vpc_id {
-  type = string
+variable "vpc_id" {
+  type        = string
+  description = "ID of the VPC where resources will be created"
 }
 
-// EFS
-variable efs_enable_encryption {
-  type    = bool
-  default = true
+variable "efs_enable_encryption" {
+  type        = bool
+  default     = true
+  description = "Whether to enable encryption for EFS"
 }
 
-variable efs_kms_key_arn {
-  type    = string
-  default = null // Defaults to aws/elasticfilesystem
-}
-
-variable efs_performance_mode {
-  type    = string
-  default = "generalPurpose" // alternative is maxIO
-}
-
-variable efs_throughput_mode {
-  type    = string
-  default = "bursting" // alternative is provisioned
-}
-
-variable efs_provisioned_throughput_in_mibps {
-  type    = number
-  default = null // might need to be 0
-}
-
-variable efs_ia_lifecycle_policy {
-  type    = string
-  default = null // Valid values are AFTER_7_DAYS AFTER_14_DAYS AFTER_30_DAYS AFTER_60_DAYS AFTER_90_DAYS
-}
-
-variable efs_subnet_ids {
-  type        = list(string)
-  description = "A list of subnets to attach to the EFS mountpoint"
+variable "efs_kms_key_arn" {
+  type        = string
   default     = null
+  description = "ARN of the KMS key to use for EFS encryption (defaults to aws/elasticfilesystem)"
 }
 
-variable efs_access_point_uid {
+variable "efs_performance_mode" {
+  type        = string
+  default     = "generalPurpose"
+  description = "Performance mode of the EFS file system (generalPurpose or maxIO)"
+}
+
+variable "efs_throughput_mode" {
+  type        = string
+  default     = "bursting"
+  description = "Throughput mode of the EFS file system (bursting or provisioned)"
+}
+
+variable "efs_provisioned_throughput_in_mibps" {
   type        = number
-  description = "The uid number to associate with the EFS access point" // Jenkins 1000
-  default     = 1000
+  default     = null
+  description = "Provisioned throughput in MiB/s for EFS when throughput mode is set to provisioned"
 }
 
-variable efs_access_point_gid {
+variable "efs_ia_lifecycle_policy" {
+  type        = string
+  default     = null
+  description = "EFS Infrequent Access (IA) lifecycle policy (AFTER_7_DAYS, AFTER_14_DAYS, AFTER_30_DAYS, AFTER_60_DAYS, AFTER_90_DAYS)"
+}
+
+variable "efs_subnet_ids" {
+  type        = list(string)
+  default     = null
+  description = "List of subnet IDs to attach to the EFS mount target"
+}
+
+variable "efs_access_point_uid" {
   type        = number
-  description = "The gid number to associate with the EFS access point" // Jenkins 1000
   default     = 1000
+  description = "User ID (uid) to associate with the EFS access point (default: 1000)"
 }
 
-variable efs_enable_backup {
-  type    = bool
-  default = true
+variable "efs_access_point_gid" {
+  type        = number
+  default     = 1000
+  description = "Group ID (gid) to associate with the EFS access point (default: 1000)"
 }
 
-variable efs_backup_schedule {
-  type    = string
-  default = "cron(0 00 * * ? *)"
+variable "efs_enable_backup" {
+  type        = bool
+  default     = true
+  description = "Whether to enable automated backups for EFS"
 }
 
-variable efs_backup_start_window {
+variable "efs_backup_schedule" {
+  type        = string
+  default     = "cron(0 00 * * ? *)"
+  description = "Cron schedule expression for EFS backup"
+}
+
+variable "efs_backup_start_window" {
   type        = number
   default     = 60
-  description = <<EOF
-A value in minutes after a backup is scheduled before a job will be
-canceled if it doesn't start successfully
-EOF
+  description = "Number of minutes to allow a backup job to start before canceling"
 }
 
-variable efs_backup_completion_window {
+variable "efs_backup_completion_window" {
   type        = number
   default     = 120
-  description = <<EOF
-A value in minutes after a backup job is successfully started before
-it must be completed or it will be canceled by AWS Backup
-EOF
+  description = "Number of minutes to allow a backup job to complete before canceling"
 }
 
-variable efs_backup_cold_storage_after_days {
+variable "efs_backup_cold_storage_after_days" {
   type        = number
   default     = 30
-  description = "Number of days until backup is moved to cold storage"
+  description = "Number of days until the EFS backup is moved to cold storage"
 }
 
-variable efs_backup_delete_after_days {
+variable "efs_backup_delete_after_days" {
   type        = number
   default     = 120
-  description = <<EOF
-Number of days until backup is deleted. If cold storage transition
-'efs_backup_cold_storage_after_days' is declared, the delete value must
-be 90 days greater
-EOF
+  description = "Number of days until the EFS backup is deleted (must be 90 days greater than cold storage transition)"
 }
 
-// alb
-variable alb_type_internal {
-  type    = bool
-  default = false
-  // default = true
-}
-
-variable alb_enable_access_logs {
-  type    = bool
-  default = false
-}
-
-variable alb_access_logs_bucket_name {
-  type    = string
-  default = null
-}
-
-variable alb_access_logs_s3_prefix {
-  type    = bool
-  default = null
-}
-
-variable alb_create_security_group {
+variable "alb_type_internal" {
   type        = bool
-  description = <<EOF
-Should a security group allowing all traffic on ports 80 * 443 be created for the alb.
-If false, a valid list of security groups must be passed with 'alb_security_group_ids'
-EOF
+  default     = false
+  description = "Whether the Application Load Balancer should be internal"
+}
+
+variable "alb_enable_access_logs" {
+  type        = bool
+  default     = false
+  description = "Whether to enable access logging for the Application Load Balancer"
+}
+
+variable "alb_access_logs_bucket_name" {
+  type        = string
+  default     = null
+  description = "Name of the S3 bucket to store ALB access logs"
+}
+
+variable "alb_access_logs_s3_prefix" {
+  type        = bool
+  default     = null
+  description = "Prefix to use for ALB access logs stored in S3"
+}
+
+variable "alb_create_security_group" {
+  type        = bool
   default     = true
+  description = "Whether to create a security group for the ALB (if false, 'alb_security_group_ids' must be provided)"
 }
 
-variable alb_security_group_ids {
+variable "alb_security_group_ids" {
   type        = list(string)
-  description = "A list of security group ids to attach to the Application Load Balancer"
   default     = null
+  description = "List of security group IDs to associate with the ALB (required if 'alb_create_security_group' is false)"
 }
 
-variable alb_ingress_allow_cidrs {
+variable "alb_ingress_allow_cidrs" {
   type        = list(string)
-  description = "A list of cidrs to allow inbound into Jenkins."
   default     = null
+  description = "List of CIDRs to allow inbound traffic to Jenkins through the ALB"
 }
 
-variable alb_subnet_ids {
+variable "alb_subnet_ids" {
   type        = list(string)
-  description = "A list of subnets for the Application Load Balancer"
   default     = null
+  description = "List of subnet IDs to associate with the ALB"
 }
 
-
-variable alb_acm_certificate_arn {
+variable "alb_acm_certificate_arn" {
   type        = string
-  description = "The ACM certificate ARN to use for the alb"
+  description = "ARN of the ACM certificate to use for HTTPS on the ALB"
 }
 
-variable jenkins_controller_port {
-  type    = number
-  default = 8080
+variable "jenkins_controller_port" {
+  type        = number
+  default     = 8080
+  description = "Port number for the Jenkins controller"
 }
 
-variable jenkins_jnlp_port {
-  type    = number
-  default = 50000
+variable "jenkins_jnlp_port" {
+  type        = number
+  default     = 50000
+  description = "Port number for the Jenkins JNLP agent"
 }
 
-variable jenkins_controller_cpu {
-  type    = number
-  default = 2048
+variable "jenkins_controller_cpu" {
+  type        = number
+  default     = 2048
+  description = "CPU units for the Jenkins controller Fargate task"
 }
 
-variable jenkins_controller_memory {
-  type    = number
-  default = 4096
+variable "jenkins_controller_memory" {
+  type        = number
+  default     = 4096
+  description = "Memory (in MiB) for the Jenkins controller Fargate task"
 }
 
-variable jenkins_controller_task_log_retention_days {
-  type    = number
-  default = 30
+variable "jenkins_controller_task_log_retention_days" {
+  type        = number
+  default     = 30
+  description = "Number of days to retain Jenkins controller task logs in CloudWatch"
 }
 
-variable jenkins_controller_subnet_ids {
+variable "jenkins_controller_subnet_ids" {
   type        = list(string)
-  description = "A list of subnets for the jenkins controller fargate service (required)"
   default     = null
+  description = "List of subnet IDs for the Jenkins controller Fargate service"
 }
 
-variable jenkins_controller_task_role_arn {
+variable "jenkins_controller_task_role_arn" {
   type        = string
-  description = "An custom task role to use for the jenkins controller (optional)"
   default     = null
+  description = "ARN of the custom IAM task role for the Jenkins controller Fargate task"
 }
 
-variable ecs_execution_role_arn {
+variable "ecs_execution_role_arn" {
   type        = string
-  description = "An custom execution role to use as the ecs exection role (optional)"
   default     = null
+  description = "ARN of the custom IAM execution role for ECS tasks"
 }
 
-// Route 53
-variable route53_create_alias {
-  type    = string
-  default = false
+variable "route53_create_alias" {
+  type        = string
+  default     = false
+  description = "Whether to create a Route 53 alias record for the ALB"
 }
 
-variable route53_zone_id {
-  type    = string
-  default = null
+variable "route53_zone_id" {
+  type        = string
+  default     = null
+  description = "ID of the Route 53 hosted zone to create the alias record in"
 }
 
-variable route53_alias_name {
-  type    = string
-  default = "jenkins-controller"
+variable "route53_alias_name" {
+  type        = string
+  default     = "jenkins-controller"
+  description = "Name of the Route 53 alias record"
 }
 
-variable tags {
-  type        = map(any)
-  description = "An object of tag key value pairs"
+variable "tags" {
+  type        = map(string)
   default     = {}
+  description = "Map of tags to assign to resources"
 }
