3e3de41afb3d507954aa96b0f1ab9d5d9f01fd9b
Thomas Vincent <thomasvincent@gmail.com>
2024-05-03 21:45:24 -0700

    refactor: Improve Jenkins initialization script using idiomatic Groovy
    The key changes include:
    - Importing only the specific required classes for better readability
    - Using `Jenkins.get()` to retrieve the Jenkins instance
    - Setting the number of executors using property assignment syntax
    - Marking the initial setup as completed using `initializeState()`
    - Retrieving environment variables using `System.getenv()`
    - Declaring variables with `def` for type inference
    - Setting the Jenkins URL using property assignment syntax
    - Saving the Jenkins configuration changes to disk

--- modules/jenkins_platform/docker/files/InitialConfig.groovy
+++ modules/jenkins_platform/docker/files/InitialConfig.groovy
@@ -1,13 +1,21 @@
-import jenkins.model.*
+import jenkins.model.Jenkins
 import jenkins.install.InstallState
-Jenkins.instance.setNumExecutors(0)
+import jenkins.model.JenkinsLocationConfiguration
 
-Jenkins.instance.setInstallState(InstallState.INITIAL_SETUP_COMPLETED)
+def jenkins = Jenkins.get()
 
+// Disable executors on the Jenkins controller
+jenkins.numExecutors = 0
 
+// Mark the initial setup as completed
+InstallState.INITIAL_SETUP_COMPLETED.initializeState()
 
-// setting the Jenkins URL
-url = System.env.JENKINS_URL
-urlConfig = JenkinsLocationConfiguration.get()
-urlConfig.setUrl(url)
-urlConfig.save()
+// Configure the Jenkins URL
+def env = System.getenv()
+def jenkinsUrl = env['JENKINS_URL']
+def locationConfig = JenkinsLocationConfiguration.get()
+locationConfig.url = jenkinsUrl
+locationConfig.save()
+
+// Save the Jenkins configuration to disk
+jenkins.save()
