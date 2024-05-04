2b0406636b18a29644c626fb912cfe9c261608c1
Thomas Vincent <thomasvincent@gmail.com>
2024-05-03 21:57:33 -0700

    Update main.tf
    more general clean up

--- example/bootstrap/main.tf
+++ example/bootstrap/main.tf
@@ -1,22 +1,21 @@
 data "aws_caller_identity" "current" {}
-data "aws_region" "current" {}
-locals {
-  account_id  = data.aws_caller_identity.current.account_id
-  region      = data.aws_region.current.name
 
+data "aws_region" "current" {}
+
+locals {
   tags = {
     team     = "devops"
     solution = "jenkins"
   }
 }
 
-resource "aws_s3_bucket" "this" {
+resource "aws_s3_bucket" "terraform_state" {
   bucket = var.state_bucket_name
 
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
-        sse_algorithm     = "AES256"
+        sse_algorithm = "AES256"
       }
     }
   }
@@ -28,19 +27,20 @@ resource "aws_s3_bucket" "this" {
   tags = local.tags
 }
 
-resource "aws_s3_bucket_public_access_block" "this" {
-  bucket                  = aws_s3_bucket.this.id
+resource "aws_s3_bucket_public_access_block" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
 
-resource "aws_dynamodb_table" "this" {
-  name = var.state_lock_table_name
-  hash_key = "LockID"
-  billing_mode     = "PAY_PER_REQUEST"
- 
+resource "aws_dynamodb_table" "terraform_state_lock" {
+  name         = var.state_lock_table_name
+  hash_key     = "LockID"
+  billing_mode = "PAY_PER_REQUEST"
+
   attribute {
     name = "LockID"
     type = "S"
@@ -49,8 +49,8 @@ resource "aws_dynamodb_table" "this" {
   server_side_encryption {
     enabled = true
   }
- 
-  # tags {
-  #   Name = "DynamoDB Terraform State Lock Table"
-  # }
+
+  tags = merge(local.tags, {
+    Name = "DynamoDB Terraform State Lock Table"
+  })
 }
